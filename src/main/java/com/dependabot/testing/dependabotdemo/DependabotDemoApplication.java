f0c33b499b488197f3ca515355951f03a12848fc
Hariharan Thavachelvam <hariharanthavachelvam@Hariharans-MacBook-Pro.local>
2024-04-15 10:56:04 +0100

    HH01: Commit everything.

---
+++ src/main/java/com/dependabot/testing/dependabotdemo/DependabotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dependabot.testing.dependabotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dependabot/testing/dependabotdemo/DependabotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dependabot.testing.dependabotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
